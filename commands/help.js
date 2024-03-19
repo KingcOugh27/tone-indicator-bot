b5bda67b31918296fc067772bbf6a88ec38ce1a3
kim <74242336+imstraightasaline@users.noreply.github.com>
2024-03-19 04:21:05 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -17,7 +17,7 @@ module.exports = {
       { name: 'General:', value: '\`help\` - Shows basic information and commands.\n\`list\` - Shows a list of tone indicators.\n\`changelog\` - Shows the changes made in the bot over time.\n\`message\` - Sends a message to me through the bot, can be used for suggestions, bug reports, etc.'},
       { name: 'Staff', value: '\`quicksetup\` - Automatically sets up the bot.\n\`setup\` - Instructions to set up the bot manually.'}
     )
-    .setFooter({ text: `Tom27#3897, Whee23#0587 | v1.3` })
+    .setFooter({ text: `#######, Whee23#0587 | v1.3` })
     .setTimestamp();
 
     return helpEmbed;
